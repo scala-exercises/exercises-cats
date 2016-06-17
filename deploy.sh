1b86e96bfd75d3aec23d107b3499b59d9a8c080a
Alejandro Gómez <alejandro@dialelo.com>
2016-06-17 10:37:29 +0200

    Run script for deploying after successful build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-echo $KEYS_PASSPHRASE | gpg --passphrase-fd 0 keys.tar.gpg
-tar xfv keys.tar.gz
-sbt compile publishSigned
+function decipherKeys{
+   echo $KEYS_PASSPHRASE | gpg --passphrase-fd 0 keys.tar.gpg
+   tar xfv keys.tar.gz
+}
+
+function publish{
+   sbt compile publishSigned
+}
+
+function release {
+    decipherKeys
+    publish
+}
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+   echo "Master branch, releasing..."
+   release
+else
+    echo "Not in master branch, skipping release"
+    # fixme: adding it here for testing in a non-master branch
+    release
+fi
+
