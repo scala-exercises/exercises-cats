4f28437104e3138e61c80b6eab8a63dce9c6dce9
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-10-17 15:52:17 +0200

    Updates optionTMonad

--- src/main/scala/catslib/Monad.scala
+++ src/main/scala/catslib/Monad.scala
@@ -106,18 +106,20 @@ object MonadSection extends FlatSpec with Matchers with org.scalaexercises.defin
     *
     * {{{
     * case class OptionT[F[_], A](value: F[Option[A]])
-    *
+
     * implicit def optionTMonad[F[_]](implicit F : Monad[F]) = {
-    * new Monad[OptionT[F, ?]] {
-    *  def pure[A](a: A): OptionT[F, A] = OptionT(F.pure(Some(a)))
-    *  def flatMap[A, B](fa: OptionT[F, A])(f: A => OptionT[F, B]): OptionT[F, B] =
-    *    OptionT {
-    *      F.flatMap(fa.value) {
-    *        case None => F.pure(None)
-    *        case Some(a) => f(a).value
-    *      }
-    *    }
-    * }
+    *   new Monad[OptionT[F, ?]] {
+    *     def pure[A](a: A): OptionT[F, A] = OptionT(F.pure(Some(a)))
+    *     def flatMap[A, B](fa: OptionT[F, A])(f: A => OptionT[F, B]): OptionT[F, B] =
+    *       OptionT {
+    *         F.flatMap(fa.value) {
+    *           case None => F.pure(None)
+    *           case Some(a) => f(a).value
+    *         }
+    *       }
+    *     def tailRecM[A, B](a: A)(f: A => OptionT[F, Either[A, B]]): OptionT[F, B] =
+    *       defaultTailRecM(a)(f)
+    *   }
     * }
     * }}}
     *
